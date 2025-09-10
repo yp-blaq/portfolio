d122ed78460837b68fb8704557ff7f82004121c0
Emmanuel Eje <85285434+yp-blaq@users.noreply.github.com>
2025-09-10 19:45:12 +0100

    Update settings.py

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -128,10 +128,18 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 

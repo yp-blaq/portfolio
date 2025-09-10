811f2501206561c01ff8a9acf01db6df4cda21eb
Emmanuel Eje <85285434+yp-blaq@users.noreply.github.com>
2025-09-10 19:42:36 +0100

    Update settings.py

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     # my apps
@@ -131,5 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
